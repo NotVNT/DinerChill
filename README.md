bd1bfcb6ee2ab912010f6e7fce42e749fa54f75b
quoctuan <tuquoctuan201@gmail.com>
2025-06-03 00:46:00 +0700

    cleanup: remove duplicate files/folders, keep only DinerChill as canonical source

--- README.md
+++
@@ -1,1 +0,0 @@
-Đồ án cơ sở năm 3. Website đặt bàn nhà hàng, quán ăn trực tuyến. Using ReactJS - NodeJs - MySQL
--- frontend/src/categories/Lau.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CategoryTemplate from './CategoryTemplate';
-
-const Lau = () => {
-  return <CategoryTemplate categoryName="Lẩu" categoryId="lau" />;
-};
-
-export default Lau;
